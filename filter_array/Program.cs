4cd68ebb12aa20432f7b91f37fcd885c8d29dc04
flovnes <whyflovnes@gmail.com>
2025-02-27 11:15:38 +0200

    first commit

---
+++ filter_array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace FilterArrayLab {
+    public class Client {
+        public delegate bool Filter(int num);
+        public static void Main() {
+            int k = int.Parse(Console.ReadLine());
+            int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Filter checkIsDivisible = x => x % k == 0; 
+
+            FilterLinq(array, checkIsDivisible);           
+            FilterManual(array, checkIsDivisible);           
+        }
+        public static int[] FilterLinq(int[] arr, Filter check) {
+            return [.. arr.Where((x) => check(x))];
+        }
+
+        
+    }
+}
---
+++ timer/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace TimerLab {
+    public class Timer {
+        private int t;
+        public delegate void targetFunction();
+        private bool active;
+        private Thread timerThread;
+        
+        public Timer(targetFunction func, int time) {
+            t = time;
+            active = true;
+            timerThread = new Thread(() => Start(func));
+            timerThread.IsBackground = true;
+            timerThread.Start();
+        }
+       
+        private void Start(targetFunction func) {
+            while (active) {
+                func();
+                Thread.Sleep(t);
+            }
+        }
+        
+        public void Stop() {
+            active = false;
+            if (timerThread.IsAlive) timerThread.Join();
+        }
+    }
+    
+    public class Client {
+        public static void MyFunc() {
+            Console.WriteLine("timer tick .");
+        }
+        
+        public static void Main() {
+            Timer timer = new(MyFunc, 1000);
+            Timer other_timer = new(MyFunc, 3000);
+            Console.WriteLine("something from the main");
+            Console.ReadLine();
+            timer.Stop();
+        }
+    }
+}
