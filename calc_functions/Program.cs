e18bb1f975fa3175bdb045f3c3d0c11bcde8d30a
flovnes <whyflovnes@gmail.com>
2025-02-27 12:06:28 +0200

    4/6

--- calc_functions/Program.cs
+++ calc_functions/Program.cs
@@ -1,1 +1,32 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace FilterArrayLab {
+    public class Client {
+        public delegate double Function(int x);
+        public static void Main() {
+            Function[] functions = new Function[3];
+
+            Function sqrtabs = x => Math.Sqrt(Math.Abs(x));
+            Function cube = x => x*x*x;
+            Function add = x => x + 3.5;
+
+            functions[0] = sqrtabs;
+            functions[1] = cube;
+            functions[2] = add; 
+            
+            Console.WriteLine("0 -- sqrt(abs(x))\n1 -- x^3\n2 -- x + 3.5\n");
+
+            while(true) {
+                try {
+                    string[] data = Console.ReadLine().Trim().Split();
+                    int choise = int.Parse(data[0]);
+                    int value = int.Parse(data[1]);
+                    Function chosenFunction = functions[choise];
+
+                    Console.WriteLine(chosenFunction(value));
+                } catch {
+                    Console.WriteLine("goodbye cruel world");
+                    break;
+                }
+            }
+        }
+    }
+}
